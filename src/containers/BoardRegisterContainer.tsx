faf0bd92059f74ba7c227b45af1941e151542e7f
jaehee <jaehee23@naver.com>
2023-04-24 08:07:45 +0900

    게시판 등록

--- src/containers/BoardRegisterContainer.tsx
+++ src/containers/BoardRegisterContainer.tsx
@@ -1,8 +1,22 @@
 import React from "react";
 import BoardRegisterForm from "../components/BoardRegisterForm";
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import * as client from "../lib/api"
 
-const BoardRegisterContainer = () => {
-    return <BoardRegisterForm/>
+const BoardRegisterContainer = ({history}: RouteComponentProps) => {
+
+    const onRegister = async (title: string, content: string, writer: string) => {
+        try{
+            const response = await client.registerBoard(title, content, writer);
+
+            alert('등록되었습니다.')
+
+        }catch (e) {
+            console.log(e);
+        }
+    };
+
+    return <BoardRegisterForm onRegister={onRegister}/>
 }
 
-export default BoardRegisterContainer;
+export default withRouter(BoardRegisterContainer);
