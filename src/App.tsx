30d8fa0f76ee43200df79ad09485173edb005f74
jaehee <jaehee23@naver.com>
2023-04-24 21:41:41 +0900

    게시글 상세보기

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ import BoardListContainer from "./containers/BoardListContainer";
 import BoardRegisterContainer from "./containers/BoardRegisterContainer";
 import BoardModifyContainer from "./containers/BoardModifyContainer";
 import BoardReadContainer from "./containers/BoardReadContainer";
+
+export interface Board {
+    readonly boardNo: string;
+    readonly title: string;
+    readonly writer: string;
+    readonly content: string;
+    readonly regDate: string;
+}
+
 function App() {
   return (
     <>
--- src/components/BoardRead.tsx
+++ src/components/BoardRead.tsx
@@ -1,50 +1,62 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../Board.module.css"
+import { Board } from "../App"
 
-function BoardRead() {
+interface Props {
+    readonly boardNo: string;
+    readonly board?: Board;
+    readonly isLoading: boolean;
+}
+
+function BoardRead({ boardNo, board, isLoading}: Props) {
     return (
         <div className={styles.centered}>
             <h2>게시판 상세보기</h2>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>번호</td>
-                        <td>
-                            <input type="text" value="100" readOnly/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>등록일시</td>
-                        <td>
-                            <input type="text" value="2020-06-05" readOnly/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>제목</td>
-                        <td>
-                            <input type="text" value="무제" readOnly/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>작성자</td>
-                        <td>
-                            <input type="text" value="홍길동" readOnly/>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>내용</td>
-                        <td>
-                            <textarea value="내용무" readOnly/>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <div className={styles.align_centered}>
-                <Link to={`/edit/100`}>편집</Link>
-                <button>삭제</button>
-                <Link to="/">목록</Link>
-            </div>
+            {isLoading && "로딩중..."}
+            {!isLoading && board && (
+              <>
+                  <table>
+                      <tbody>
+                      <tr>
+                          <td>번호</td>
+                          <td>
+                              <input type="text" value={board.boardNo} readOnly/>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>등록일시</td>
+                          <td>
+                              <input type="text" value={board.regDate} readOnly/>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>제목</td>
+                          <td>
+                              <input type="text" value={board.title} readOnly/>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>작성자</td>
+                          <td>
+                              <input type="text" value={board.writer} readOnly/>
+                          </td>
+                      </tr>
+                      <tr>
+                          <td>내용</td>
+                          <td>
+                              <textarea value={board.content} readOnly/>
+                          </td>
+                      </tr>
+                      </tbody>
+                  </table>
+                  <div className={styles.align_centered}>
+                      <Link to={`/edit/${boardNo}`}>편집</Link>
+                      <button>삭제</button>
+                      <Link to="/">목록</Link>
+                  </div>
+              </>
+            )}
         </div>
     )
 }
