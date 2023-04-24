244371c0f3ec0be8c7b7796005b765abc5043dd1
jaehee <jaehee23@naver.com>
2023-04-24 21:51:46 +0900

    게시글 목록

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const registerBoard = (title: string, content: string, writer: string) =>
     axios.post("/boards", {title, content, writer});
-
 export const fetchBoard = (boardNo: string) => axios.get(`/boards/${boardNo}`);
+export const fetchBoardList = () => axios.get("/boards");
+
