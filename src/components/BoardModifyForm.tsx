f3bdced65299a6eb323ff992d78ab0bf4bf2b822
jaehee <jaehee23@naver.com>
2023-04-25 13:12:52 +0900

    게시판 수정

--- src/components/BoardModifyForm.tsx
+++ src/components/BoardModifyForm.tsx
@@ -1,52 +1,100 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom"
 import styles from "../Board.module.css"
 
-function BoardModifyForm(){
+import { Board } from "../App"
+
+interface Props {
+    readonly board?:Board;
+    readonly isLoading: boolean;
+    readonly onModify: (boardNo: string, title: string, content: string) => void;
+}
+
+
+function BoardModifyForm({board, isLoading, onModify}: Props){
+
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+
+    const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    };
+
+    const handleChangeContent = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setContent(e.target.value);
+    }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if(!!board){
+            onModify(board.boardNo, title, content);
+        }
+    }
+
+    useEffect(()=>{
+        console.log("useEffect board =" + board);
+
+        if(board){
+            console.log("board.title" + board.title);
+            console.log("board.content" + board.content);
+
+            setTitle(board.title)
+            setContent(board.content)
+        }
+    }, [board])
+
+
     return (
         <div className={styles.centered}>
             <h2>게시판수정</h2>
-            <form>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>번호</td>
-                            <td>
-                                <input value="100" type="text" disabled/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>등록일시</td>
-                            <td>
-                                <input value="2020-06-05" type="text" disabled/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>제목</td>
-                            <td>
-                                <input type="text"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>작성자</td>
-                            <td>
-                                <input type="text" value="홍길동" disabled/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>내용</td>
-                            <td>
-                                <textarea rows={5}></textarea>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            {isLoading && "로딩중 ..."}
+            {!isLoading && board && (
+                <>
+                    <form onSubmit={handleSubmit}>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td>번호</td>
+                                <td>
+                                    <input value={board.boardNo} type="text" disabled/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>등록일시</td>
+                                <td>
+                                    <input value={board.regDate} type="text" disabled/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>제목</td>
+                                <td>
+                                    <input type="text" value={title} onChange={handleChangeTitle}/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>작성자</td>
+                                <td>
+                                    <input type="text" value={board.writer} disabled/>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>내용</td>
+                                <td>
+                                    <textarea value={content} rows={5} onChange={handleChangeContent}></textarea>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
 
-                <div className={styles.align_centered}>
-                    <button type="submit">수정</button>
-                    <Link to={`/read/100`}>취소</Link>
-                </div>
-            </form>
+                        <div className={styles.align_centered}>
+                            <button type="submit">수정</button>
+                            <Link to={`/read/${board.boardNo}`}>취소</Link>
+                        </div>
+                    </form>
+                </>
+            )}
+
         </div>
     )
 }
